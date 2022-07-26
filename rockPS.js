7f0a2f8319fda8cdf3e26cb0df0dcc95688c64e8
sinarin <alexngdragon@hotmail.com>
2022-07-26 16:52:09 -0400

    complete game v1, take out changed alerts to console.log

--- rockPS.js
+++ rockPS.js
@@ -6,30 +6,28 @@ let pcScore = 0;
 for(let i = 0; i < 5; i++){
   let rResult = round();
   if (typeof rResult === "string"){
+    console.log(rResult)
     if (rResult.slice(0, 7) === "You Win"){
       userScore++;
-      alert(`You: ${userScore} Computer: ${pcScore}`);
+      console.log(`You: ${userScore} Computer: ${pcScore}`);
     }
-    if(rResult.slice(0, 8) === "You Lose"){
+    else if(rResult.slice(0, 8) === "You Lose"){
       pcScore++;
-      alert(`You: ${userScore} Computer: ${pcScore}`);
+      console.log(`You: ${userScore} Computer: ${pcScore}`);
     }
     else {
-      return(`You: ${userScore} Computer: ${pcScore}`);
+      console.log(`You: ${userScore} Computer: ${pcScore}`);
     }
   }
 }
 if (userScore > pcScore){
-  alert(`You: ${userScore} Computer: ${pcScore}
-You Win the match!`);
+  return(`You: ${userScore} Computer: ${pcScore}. You Win the match!`);
 }
 else if (pcScore > userScore){
-  alert(`You: ${userScore} Computer: ${pcScore}
-You Lose the match!`);
+  return(`You: ${userScore} Computer: ${pcScore}. You Lose the match!`);
 }
 else{
-  alert(`You: ${userScore} Computer: ${pcScore}
-The Match is a Tie!`);
+  return(`You: ${userScore} Computer: ${pcScore}. The Match is a Tie!`);
 }
 
 }
@@ -66,37 +64,31 @@ function round () {
 
   if (userChoice === computerChoice)
   {
-    alert(`Tie!, ${userChoice} ties ${computerChoice}`);
-    return(`Tie!, ${userChoice} ties ${computerChoice}`);
+    return(`Tie!, ${userChoice} ties ${computerChoice}`);  
   }
   else{
     switch (userChoice){
       case "rock":
         switch (computerChoice){
           case "paper":
-            alert("You Lose, rock gets eaten by paper!");
             return("You Lose, rock gets eaten by paper!");
-            break;
           default:
-            alert("You Win, rock smashes scissors!");
             return("You Win, rock smashes scissors!");
-        }break;
+        }
       case "paper":
         switch (computerChoice){
           case "rock":
-            alert("You Win, paper eats rock!");
-            break;
+            return("You Win, paper eats rock!");
           default:
-            alert("You Lose, paper gets cut up by scissors!");
-        }break;
+            return("You Lose, paper gets cut up by scissors!");
+        }
       //scissors option below
       default:
         switch (computerChoice){
           case "rock":
-            alert("You Lose, scissors gets smashes by rock!");
-            break;
+            return("You Lose, scissors gets smashes by rock!");
           default:
-            alert("You Win, scissors cuts up paper!");
+            return("You Win, scissors cuts up paper!");
         }
     }
   }
